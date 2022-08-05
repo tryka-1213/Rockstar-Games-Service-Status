b7148fcef755991c45ab301fbfa34e09fee40612
tryka-1213 <107329072+tryka-1213@users.noreply.github.com>
2022-08-05 03:45:49 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+const axios = require("axios")
+
+module.exports = {
+    async getStatus(){
+        return new Promise(async (resolve, reject) => {
+            try {
+                var response = await axios({ method: "get", url: "https://support.rockstargames.com/services/status.json?tz=Europe/London" })
+            } catch {
+                // Reject the promise on error
+                return reject("Request to Rockstar Games Service Status was unsuccessful. This may be because the IP of this machine is blacklisted or temporarily banned")
+            }
+            
+            let base = response.data.statuses
+            let updatedAt = new Date(response.data.updated.replace(", ", " ").replace(" at ", " "))
+
+            const status = {
+                redDeadOnline: {
+                    "pc": base[1].services_platforms.find(s => s.name == "PC").service_status.status,
+                    "xb1": base[1].services_platforms.find(s => s.name == "Xbox One").service_status.status,
+                    "xbox_cloud_gaming": base[1].services_platforms.find(s => s.name == "Xbox Cloud Gaming").service_status.status,
+                    "stadia": base[1].services_platforms.find(s => s.name == "Stadia").service_status.status,
+                    "ps4": base[1].services_platforms.find(s => s.name == "PS4").service_status.status
+                },
+                gtaOnline: {
+                    "pc": base[2].services_platforms.find(s => s.name == "PC").service_status.status,
+                    "xb1": base[2].services_platforms.find(s => s.name == "Xbox One").service_status.status,
+                    "xbox_series_xs": base[2].services_platforms.find(s => s.name == "Xbox Series X/S").service_status.status,
+                    "xbox_cloud_gaming": base[2].services_platforms.find(s => s.name == "Xbox Cloud Gaming").service_status.status,
+                    "ps4": base[2].services_platforms.find(s => s.name == "PS4").service_status.status,
+                    "ps5": base[2].services_platforms.find(s => s.name == "PS5").service_status.status  
+                },
+                socialClub: {
+                    "all": base[3].services_platforms[0].service_status.status
+                },
+                launcher: {
+                    "auth": base[5].services_platforms.find(s => s.name == "Authentication").service_status.status,
+                    "store": base[5].services_platforms.find(s => s.name == "Store").service_status.status,
+                    "downloads": base[5].services_platforms.find(s => s.name == "Downloads").service_status.status,
+                    "cloud_downloads": base[5].services_platforms.find(s => s.name == "Cloud Services").service_status.status
+                },
+                lastUpdated: {
+                    "full": updatedAt,
+                    "timestamp": Math.round(updatedAt.getTime() / 1000)
+            
+                }
+            }
+            
+            // Resolve the promise
+            resolve(status) 
+
+        })
+
+    }
+}
